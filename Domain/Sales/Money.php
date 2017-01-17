6f41115bb36f6d245207ea9d53fa765273d01393
James Mannion <mannion007@gmail.com>
2017-01-17 16:35:13 +0000

    Implemented Money objects

--- Domain/Sales/Money.php
+++ Domain/Sales/Money.php
@@ -4,6 +4,40 @@ namespace Mannion007\BestInvestments\Domain\Sales;
 
 class Money
 {
-    private $currency;
     private $amount;
+
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+    
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function add(Money $other)
+    {
+        return new static($this->amount + $other->getAmount());
+    }
+
+    public function subtract(Money $other)
+    {
+        return new static($this->amount - $other->getAmount());
+    }
+
+    public function isMoreThan(Money $other)
+    {
+        return $this->amount > $other;
+    }
+
+    public function isLessThan(Money $other)
+    {
+        return $this->amount < $other;
+    }
+
+    public function __toString()
+    {
+        return (string)$this->amount;
+    }
 }
