d5e88cc7ea53a956cfa51b6145fd7b2b98a588db
James Mannion <mannion007@gmail.com>
2017-03-06 10:55:56 +0000

    Updated Money value object in the invoicing context so that it cannot be a negative amount and added spec tests.

--- src/Invoicing/Domain/Money.php
+++ src/Invoicing/Domain/Money.php
@@ -5,10 +5,15 @@ namespace Mannion007\BestInvestments\Invoicing\Domain;
 class Money
 {
     private $amount;
+    private $currency;
 
-    public function __construct(int $amount)
+    public function __construct(int $amount, Currency $currency)
     {
+        if ($amount < 0) {
+            throw new \InvalidArgumentException('Cannot create a negative amount of money');
+        }
         $this->amount = $amount;
+        $this->currency = $currency;
     }
 
     public function getAmount(): int
@@ -16,28 +21,54 @@ class Money
         return $this->amount;
     }
 
+    public function getCurrency(): Currency
+    {
+        return $this->currency;
+    }
+
     public function add(Money $other): Money
     {
-        return new static($this->amount + $other->getAmount());
+        return new static($this->amount + $other->getAmount(), $this->currency);
     }
 
     public function subtract(Money $other): Money
     {
-        return new static($this->amount - $other->getAmount());
+        if ($other->getCurrency()->isNot($this->getCurrency())) {
+            throw new \InvalidArgumentException('Cannot subtract because currencies do not match');
+        }
+        if ($this->amount - $other->getAmount() < 0) {
+            throw new \InvalidArgumentException(
+                'Cannot subtract an amount that would result in a negative amount of money'
+            );
+        }
+        return new static($this->amount - $other->getAmount(), $this->currency);
     }
 
     public function isMoreThan(Money $other): bool
     {
-        return $this->amount > $other;
+        return $this->compareWith($other) === 1;
     }
 
     public function isLessThan(Money $other): bool
     {
-        return $this->amount < $other;
+        return $this->compareWith($other) < 1;
+    }
+
+    private function compareWith(Money $other): int
+    {
+        if ($other->getCurrency()->isNot($this->getCurrency())) {
+            throw new \Exception('Cannot compare because currencies do not match');
+        }
+        return $this->getAmount() <=> $other->getAmount();
     }
 
     public function __toString()
     {
-        return (string)$this->amount;
+        return sprintf(
+            "%s %s.%s",
+            strtoupper((string)$this->currency),
+            substr($this->amount, 0, strlen($this->amount) - 2),
+            substr($this->amount, -1 * 2)
+        );
     }
 }
