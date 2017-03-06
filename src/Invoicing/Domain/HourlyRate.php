5f69f80193eb0ade41d15b628ce6c0a1c4bf0b29
James Mannion <mannion007@gmail.com>
2017-03-06 12:17:31 +0000

    Removed generic money object from invoicing in place of objects which use the language of the domain

---
+++ src/Invoicing/Domain/HourlyRate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mannion007\BestInvestments\Invoicing\Domain;
+
+use Mannion007\ValueObjects\Money;
+use Mannion007\ValueObjects\Currency;
+
+class HourlyRate extends Money
+{
+    public function __construct(int $amount, Currency $currency)
+    {
+        if ($currency->isNot(Currency::gbp())) {
+            throw new \InvalidArgumentException('Only GBP currency is allowed');
+        }
+        parent::__construct($amount, $currency);
+    }
+}
